257e1b00c4157d58aaf010b46ef0d19334da0e56
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2020-10-13 16:19:10 -0300

    Actualización primera

---
+++ src/components/calendar/CalendarScreen.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "moment/locale/es";
+import { Navbar } from "../ui/Navbar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { messages } from "../../helpers/calendar-messages-es";
+import { CalendarEvent } from "./CalendarEvent";
+import { CalendarModal } from "./CalendarModal";
+import { useDispatch, useSelector } from "react-redux";
+import { uiOpenModal } from "../../actions/ui";
+import { eventClearActiveEvent, eventSetActive } from "../../actions/events";
+import { AddNewFab } from "../ui/AddNewFab";
+import { DeleteEventFab } from "../ui/DeleteEventFab";
+
+moment.locale("es");
+
+const localizer = momentLocalizer(moment);
+
+export const CalendarScreen = () => {
+  const dispatch = useDispatch();
+  const { events, activeEvent } = useSelector((state) => state.calendar);
+
+  const [lastView, setLastView] = useState(
+    localStorage.getItem("lastView") || "month"
+  );
+
+  const onDoubleClick = (e) => {
+    // console.log(e);
+    dispatch(uiOpenModal());
+  };
+
+  const onSelectEvent = (e) => {
+    dispatch(eventSetActive(e));
+    //dispatch(uiOpenModal());
+  };
+
+  const onViewChange = (e) => {
+    setLastView(e);
+    localStorage.setItem("lastView", e);
+  };
+
+  const onSelectSlot = (e) => {
+    //console.log(e);
+    dispatch(eventClearActiveEvent());
+  };
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#367CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      display: "block",
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+
+  return (
+    <div className="calendar-screen">
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        onSelectSlot={onSelectSlot}
+        selectable={true}
+        view={lastView}
+        components={{
+          event: CalendarEvent,
+        }}
+      />
+
+      <AddNewFab />
+
+      {activeEvent && <DeleteEventFab />}
+
+      <CalendarModal />
+    </div>
+  );
+};
---
+++ src/types/types.js
@@ -0,0 +1,10 @@
+export const types = {
+  uiOpenModal: "[ui] Open modal",
+  uiCloseModal: "[ui] Close modal",
+
+  eventSetActive: "[event] Set Active",
+  eventAddNew: "[event] Add New",
+  eventClearActiveEvent: "[event] Clear active event",
+  eventUpdated: "[event] Event updated",
+  eventDeleted: "[event] Event deleted",
+};
