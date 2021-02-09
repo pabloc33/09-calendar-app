e8ebb574866765f516ce8b6d68cd178338788b49
pabloc33 <68171460+pabloc33@users.noreply.github.com>
2021-02-09 20:39:00 -0300

    Actualización segunda

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import { calendarReducer } from "./calendarReducer";
 import { uiReducer } from "./uiReducer";
+import { authReducer } from "./authReducer";
 
 export const rootReducer = combineReducers({
   ui: uiReducer,
   calendar: calendarReducer,
-  //TODO: AuthReducer
+  auth: authReducer,
 });
